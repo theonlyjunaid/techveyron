de5ec0077700dc53bff3513335ccc54e89fdcabb
Junaid <junaid.@Junaids-MacBook-Air.local>
2022-08-16 15:11:34 +0530

    rfgd

--- components/PostDetails.jsx
+++ components/PostDetails.jsx
@@ -42,7 +42,7 @@ export default function PostDetails({ post }) {
     };
 
     return (
-        <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8'>
+        <div className='bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8 '>
             <div className="relative overflow-hidden shadow-md mb-6 justify-center">
                 <img src={post.featuredImage.url} alt={post.title} className='object-top h-full  w-full rounded-t-lg' />
             </div>
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -14,7 +14,7 @@ export default function Slug({ post }) {
     }
     return (
 
-        <div className='container mx-auto px-10 mb-8'>
+        <div className='container mx-auto px-4 mb-8'>
             <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
                 <div className='col-span-1 lg:col-span-8'>
                     <PostDetails post={post} />
