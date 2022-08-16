815b3acbc7507de83090290f3b411fc479df2928
Junaid <junaid.@Junaids-MacBook-Air.local>
2022-08-16 15:07:11 +0530

    dsz

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,38 +1,38 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import { GetStaticPaths, GetStaticProps } from 'next'
+// import { getStaticPaths, getStaticProps } from 'next'
 import { getPosts, getPostDetails } from '../../services'
 import { PostDetails, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
- 
+
 
 
 export default function Slug({ post }) {
 
-const router = useRouter();
-if (router.isFallback) {
-    return <Loader />
-} 
-return (
+    const router = useRouter();
+    if (router.isFallback) {
+        return <Loader />
+    }
+    return (
 
-    <div className='container mx-auto px-10 mb-8'>
-        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-            <div className='col-span-1 lg:col-span-8'>
-                <PostDetails post={post} />
-                <Author author={post.author} />
-                <CommentsForm slug={post.slug} />
-                <Comments slug={post.slug} />
-            </div>
-            <div className='col-span-1 lg:col-span-4'>
-                <div className="relative lg:sticky top-8">
-                    <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
-                    <Categories />
+        <div className='container mx-auto px-10 mb-8'>
+            <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+                <div className='col-span-1 lg:col-span-8'>
+                    <PostDetails post={post} />
+                    <Author author={post.author} />
+                    <CommentsForm slug={post.slug} />
+                    <Comments slug={post.slug} />
+                </div>
+                <div className='col-span-1 lg:col-span-4'>
+                    <div className="relative lg:sticky top-8">
+                        <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+                        <Categories />
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-)
+    )
 }
 export async function getStaticProps({ params }) {
     const data = await getPostDetails(params.slug)
